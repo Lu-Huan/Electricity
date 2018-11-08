efcbc7fed93ce8eaeb10c498e63ca21e50b067fd
Lu-Huan <180356757@qq.com>
2018-11-08 18:44:52 +0800

    3

---
+++ Assets/AttackBot/attackBot.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class attackBot : MonoBehaviour {
+
+    private Transform rightWheel;
+    private Transform leftWheel;
+    // Use this for initialization
+    void Start () {
+        rightWheel = transform.Find("rightWheel").transform;
+        leftWheel = transform.Find("leftWheel").transform;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        rightWheel.Rotate(0, -1, 0);
+        leftWheel.Rotate(0, 1, 0);
+    }
+}
